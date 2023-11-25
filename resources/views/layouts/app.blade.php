fb0ffceea47be261e2f97893f94adef237620372
JeyanJegatheeswaran <150778658+JeyanJegatheeswaran@users.noreply.github.com>
2023-11-25 16:14:25 +0530

    Laravel CRUD App
    Its phone Image and Details Create, Read, Upload and Delete

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+
+    <title>Laravel CRUD</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}"/>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand bg-black">
+        <div class="container-fluid">
+            <a href="/" class="navbar-brand text-light">Laravel CRUD</a>
+        </div>
+    </nav>
+    <div class="container mt-5">
+        <div class="row">
+            @if(session('success'))
+                <div class="alert alert-success alert-dismissible fade show">
+                    <strong>Success:</strong> {{ session('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+            @yield('main')
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
